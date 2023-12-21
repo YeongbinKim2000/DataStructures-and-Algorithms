8b95fc820a8a8da746c007a91504bc458090cae7
YeongbinKim2000 <133650689+YeongbinKim2000@users.noreply.github.com>
2023-12-20 22:24:23 -0500

    Add files via upload

---
+++ DataStructures/HashMap/QuadraticProbingHashMap.java
@@ -0,0 +1,370 @@
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+public class QuadraticProbingHashMap<K, V> {
+
+    /**
+     * The initial capacity of the QuadraticProbingHashMap when created with the
+     * default constructor.
+     *
+     */
+    public static final int INITIAL_CAPACITY = 13;
+
+    /**
+     * The max load factor of the QuadraticProbingHashMap
+     *
+     */
+    private static final double MAX_LOAD_FACTOR = 0.67;
+
+    // Do not add new instance variables or modify existing ones.
+    private QuadraticProbingMapEntry<K, V>[] table;
+    private int size;
+
+    /**
+     * Constructs a new QuadraticProbingHashMap.
+     *
+     * The backing array should have an initial capacity of INITIAL_CAPACITY.
+     *
+     * Use constructor chaining.
+     */
+    public QuadraticProbingHashMap() {
+        this(INITIAL_CAPACITY);
+    }
+
+    /**
+     * Constructs a new QuadraticProbingHashMap.
+     *
+     * The backing array should have an initial capacity of initialCapacity.
+     *
+     * You may assume initialCapacity will always be positive.
+     *
+     * @param initialCapacity the initial capacity of the backing array
+     */
+    public QuadraticProbingHashMap(int initialCapacity) {
+        table = (QuadraticProbingMapEntry<K, V>[]) new QuadraticProbingMapEntry[initialCapacity];
+        size = 0;
+    }
+
+    /**
+     * Adds the given key-value pair to the map. If an entry in the map
+     * already has this key, replace the entry's value with the new one
+     * passed in.
+     *
+     * In the case of a collision, use quadratic probing as your resolution
+     * strategy.
+     *
+     * Before actually adding any data to the HashMap, you should check to
+     * see if the array would violate the max load factor if the data was
+     * added. For example, let's say the array is of length 5 and the current
+     * size is 3 (LF = 0.6). For this example, assume that no elements are
+     * removed in between steps. If another entry is attempted to be added,
+     * before doing anything else, you should check whether (3 + 1) / 5 = 0.8
+     * is larger than the max LF. It is, so you would trigger a resize before
+     * you even attempt to add the data or figure out if it's a duplicate. Be
+     * careful to consider the differences between integer and double
+     * division when calculating load factor.
+     *
+     * You must also resize when there are not any valid spots to add a
+     * (key, value) pair in the HashMap after checking table.length spots.
+     * There is more information regarding this case in the assignment PDF.
+     *
+     * When regrowing, resize the length of the backing table to
+     * 2 * old length + 1. You must use the resizeBackingTable method to do so.
+     *
+     * Return null if the key was not already in the map. If it was in the map,
+     * return the old value associated with it.
+     *
+     * @param key the key to add
+     * @param value the value to add
+     * @return null if the key was not already in the map. If it was in the
+     * map, return the old value associated with it
+     * @throws java.lang.IllegalArgumentException if key or value is null
+     */
+    public V put(K key, V value) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key is null");
+        } else if (value == null) {
+            throw new IllegalArgumentException("The value is null");
+        }
+
+        if ((size + 1.0) / table.length > MAX_LOAD_FACTOR) {
+            resizeBackingTable(table.length * 2 + 1);
+        }
+
+        QuadraticProbingMapEntry<K, V> replace = new QuadraticProbingMapEntry<>(key, value);
+        int hashCode = Math.abs(key.hashCode() % table.length);
+        int index = hashCode;
+        int removedIdx = 0;
+        int probeCnt = 0;
+        boolean check = true;
+        while (probeCnt < table.length && table[index] != null) {
+            if (table[index].getKey().equals(key)) {
+                if (!table[index].isRemoved()) {
+                    V data = table[index].getValue();
+                    table[index].setValue(value);
+                    return data;
+                } else {
+                    table[index] = replace;
+                    size++;
+                    return null;
+                }
+            } else if (table[index].isRemoved() && check) {
+                removedIdx = index;
+                check = false;
+            } else {
+                probeCnt++;
+                if (probeCnt < table.length) {
+                    index = (hashCode + (probeCnt * probeCnt)) % table.length;
+                }
+            }
+        }
+        if (!check) {
+            table[removedIdx] = replace;
+            size++;
+            return null;
+        } else if (table[index] == null) {
+            table[index] = replace;
+            size++;
+            return null;
+        } else {
+            resizeBackingTable(2 * table.length + 1);
+            return put(key, value);
+        }
+    }
+
+    /**
+     * Removes the entry with a matching key from map by marking the entry as
+     * removed.
+     *
+     * @param key the key to remove
+     * @return the value previously associated with the key
+     * @throws java.lang.IllegalArgumentException if key is null
+     * @throws java.util.NoSuchElementException if the key is not in the map
+     */
+    public V remove(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Cannot remove null key");
+        }
+        int hashCode = Math.abs(key.hashCode() % table.length);
+        int index = hashCode;
+        V removed = null;
+        int probeCnt = 0;
+        for (probeCnt = 0; probeCnt < table.length; probeCnt++) {
+            index = (probeCnt * probeCnt + hashCode) % table.length;
+            if (table[index] != null) {
+                if (table[index].isRemoved() && table[index].getKey().equals(key)) {
+                    throw new NoSuchElementException("The key doesn't exist in the map");
+                } else if (!table[index].isRemoved() && table[index].getKey().equals(key)) {
+                    removed = table[index].getValue();
+                    table[index].setRemoved(true);
+                    size--;
+                    return removed;
+                }
+            } else {
+                throw new NoSuchElementException("The key doesn't exist in the map");
+            }
+        }
+        if (probeCnt == table.length) {
+            throw new NoSuchElementException("The key doesn't exist in the map");
+        }
+        return removed;
+    }
+
+    /**
+     * Gets the value associated with the given key.
+     *
+     * @param key the key to search for in the map
+     * @return the value associated with the given key
+     * @throws java.lang.IllegalArgumentException if key is null
+     * @throws java.util.NoSuchElementException if the key is not in the map
+     */
+    public V get(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("The key is null");
+        }
+        int hashCode = Math.abs(key.hashCode() % table.length);
+        V getValue = null;
+        for (int i = 0; i < table.length; i++) {
+            int index = (i * i + hashCode) % table.length;
+            if (table[index] != null) {
+                if (table[index].getKey().equals(key)) {
+                    if (!table[index].isRemoved()) {
+                        getValue = table[index].getValue();
+                        return getValue;
+                    } else {
+                        throw new NoSuchElementException("The key doesn't exist in the map");
+                    }
+                }
+            } else {
+                throw new NoSuchElementException("The key doesn't exist in the map");
+            }
+        }
+        throw new NoSuchElementException("The key doesn't exist in the map");
+    }
+
+    /**
+     * Returns whether or not the key is in the map.
+     *
+     * @param key the key to search for in the map
+     * @return true if the key is contained within the map, false
+     * otherwise
+     * @throws java.lang.IllegalArgumentException if key is null
+     */
+    public boolean containsKey(K key) {
+        if (key == null) {
+            throw new IllegalArgumentException("Cannot contain null key");
+        }
+
+        int index = Math.abs(key.hashCode() % table.length);
+        int curIdx;
+        int quadratic = 0;
+        for (int i = 0; i < table.length; i++) {
+            curIdx = (quadratic * quadratic + index) % table.length;
+            quadratic++;
+            if (table[curIdx] != null) {
+                if (!table[curIdx].isRemoved() && table[curIdx].getKey().equals(key)) {
+                    return true;
+                } else if (table[curIdx].isRemoved() && table[curIdx].getKey().equals(key)) {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Returns a Set view of the keys contained in this map.
+     *
+     * Use java.util.HashSet.
+     *
+     * @return the set of keys in this map
+     */
+    public Set<K> keySet() {
+        Set<K> keySet = new HashSet<>();
+        if (size == 0) {
+            return keySet;
+        }
+        int cnt = 0;
+        for (QuadraticProbingMapEntry<K, V> entry : table) {
+            if (entry != null && !entry.isRemoved()) {
+                keySet.add(entry.getKey());
+                cnt++;
+            }
+            if (cnt == size) {
+                break;
+            }
+        }
+        return keySet;
+    }
+
+    /**
+     * Returns a List view of the values contained in this map.
+     *
+     * Use java.util.ArrayList or java.util.LinkedList.
+     *
+     * You should iterate over the table in order of increasing index and add
+     * entries to the List in the order in which they are traversed.
+     *
+     * @return list of values in this map
+     */
+    public List<V> values() {
+        List<V> valueList = new ArrayList<>();
+        if (size == 0) {
+            return valueList;
+        }
+        int cnt = 0;
+        for (QuadraticProbingMapEntry<K, V> entry : table) {
+            if (entry != null && !entry.isRemoved()) {
+                valueList.add(entry.getValue());
+                cnt++;
+            }
+            if (cnt == size) {
+                break;
+            }
+        }
+        return valueList;
+    }
+
+    /**
+     * Resize the backing table to length.
+     *
+     * Disregard the load factor for this method. So, if the passed in length is
+     * smaller than the current capacity, and this new length causes the table's
+     * load factor to exceed MAX_LOAD_FACTOR, you should still resize the table
+     * to the specified length and leave it at that capacity.
+     *
+     * Note: This method does not have to handle the case where the new length
+     * results in collisions that cannot be resolved without resizing again. It
+     * also does not have to handle the case where size = 0, and length = 0 is
+     * passed into the function.
+     *
+     * You should iterate over the old table in order of increasing index and
+     * add entries to the new table in the order in which they are traversed.
+     *
+     * Since resizing the backing table is working with the non-duplicate
+     * data already in the table, you shouldn't explicitly check for
+     * duplicates.
+     *
+     * @param length new length of the backing table
+     * @throws java.lang.IllegalArgumentException if length is less than the
+     * number of items in the hash map
+     */
+    public void resizeBackingTable(int length) {
+        if (length < size) {
+            throw new IllegalArgumentException("The length must be greater than the size");
+        }
+        QuadraticProbingMapEntry<K, V>[] temp = (QuadraticProbingMapEntry<K, V>[]) new QuadraticProbingMapEntry[length];
+        for (int i = 0; i < table.length; i++) {
+            if (table[i] != null) {
+                int hashCode = Math.abs((table[i].getKey()).hashCode() % length);
+                int index = hashCode;
+                int h = 0;
+                while (temp[index] != null) {
+                    h++;
+                    index = (h * h + hashCode) % length;
+                }
+                if (!table[i].isRemoved()) {
+                    temp[index] = table[i];
+                }
+            }
+        }
+        table = temp;
+    }
+
+    /**
+     * Clears the map.
+     *
+     * Resets the table to a new array of the INITIAL_CAPACITY and resets the
+     * size.
+     *
+     * Must be O(1).
+     */
+    public void clear() {
+        table = (QuadraticProbingMapEntry<K, V>[]) new QuadraticProbingMapEntry[INITIAL_CAPACITY];
+        size = 0;
+    }
+
+    /**
+     * Returns the table of the map.
+     *
+     * @return the table of the map
+     */
+    public QuadraticProbingMapEntry<K, V>[] getTable() {
+        return table;
+    }
+
+    /**
+     * Returns the size of the map.
+     *
+     * @return the size of the map
+     */
+    public int size() {
+        return size;
+    }
+}
